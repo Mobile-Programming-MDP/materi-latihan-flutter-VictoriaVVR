9a508def9054cf506f6d254dcd0f256b4b717ee8
VictoriaVVR <victoriavalensitarobert_2226250110@mhs.mdp.ac.id>
2023-10-13 18:36:39 +0700

    Menambahkan icon di appbar

---
+++ hello_widget/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    title: "Hallo Widget",
+    theme: ThemeData(primarySwatch: Colors.blue),
+    home: FirstScreen());
+  }
+}  
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
+
+@override 
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+    title: const Text("Victoria"),
+    actions: <Widget> [
+      IconButton(
+      onPressed: () {}, 
+      icon: const Icon(
+        Icons.search,
+        color: Colors.white,
+        ))
+    ],
+    leading: IconButton (
+    onPressed: () {},
+    icon: const Icon(
+      Icons.home,
+      color: Colors.white,
+    )),
+  ),
+  body: const Center(child: Text("Hello Victoria"),
+  ),
+  );
+}
+}
