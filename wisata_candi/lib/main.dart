c1ee15ed41ede8e3f8d6fa14fd7f90ad87e904a3
VictoriaVVR <victoriavalensitarobert_2226250110@mhs.mdp.ac.id>
2023-11-13 18:07:18 +0700

    Routing

--- wisata_candi/lib/main.dart
+++ wisata_candi/lib/main.dart
@@ -3,8 +3,8 @@ import 'package:wisata_candi/data/candi_data.dart';
 import 'package:wisata_candi/screens/detail_screen.dart';
 import 'package:wisata_candi/screens/home_screen.dart';
 import 'package:wisata_candi/screens/profile_screen.dart';
+import 'package:wisata_candi/screens/favorite_screen.dart';
 import 'package:wisata_candi/screens/search_screen.dart';
-import 'package:wisata_candi/screens/sign_in.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,27 +17,82 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Wisata Candi',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(
-          iconTheme: IconThemeData(color: Colors.deepPurple),
-          titleTextStyle: TextStyle(
-            color: Colors.deepPurple,
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-          ),
+      title: 'Wisata Candi',
+      theme: ThemeData(),
+      home: MainScreen(),
+      // home: DetailScreen(candi: candiList[0]),
+      //home: ProfileScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  //TODO: Deklarasikan variabel
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    HomeScreen(),
+    SearchScreen(),
+    FavoriteScreen(),
+    ProfileScreen(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //TODO:2. Buat properti body berupa widget yang ditampilkan
+      body: _children[_currentIndex],
+      //TODO:3. buat properti bottomNavigationBar dengan nilai THeme
+      bottomNavigationBar: Theme(
+        //TODO:4. Buat data dan child dari Theme
+        data: Theme.of(context).copyWith(
+          canvasColor: Colors.deepPurple[50],
         ),
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple).copyWith(
-          primary: Colors.deepPurple,
-          surface: Colors.deepPurple[50],
+        child: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+                color: Colors.deepPurple,
+              ),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.search,
+                color: Colors.deepPurple,
+              ),
+              label: 'Search',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.favorite,
+                color: Colors.deepPurple,
+              ),
+              label: 'Favorite',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person, color: Colors.deepPurple),
+              label: 'Profile',
+            ),
+          ],
+          selectedItemColor: Colors.deepPurple,
+          unselectedItemColor: Colors.deepPurple[100],
+          showUnselectedLabels: true,
         ),
-        useMaterial3: true,
       ),
-      // home: ProfileScreen(),
-      //home: DetailScreen(candi: candiList[0]),
-        // home: SignInScreen(),
-        home: SearchScreen(),
     );
   }
 }
