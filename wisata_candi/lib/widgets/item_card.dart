5227ad64f56fac3d8c32e10aefc7f53a4416109d
VictoriaVVR <victoriavalensitarobert_2226250110@mhs.mdp.ac.id>
2023-11-17 14:49:10 +0700

    tombol back

--- wisata_candi/lib/widgets/item_card.dart
+++ wisata_candi/lib/widgets/item_card.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:wisata_candi/models/candi.dart';
+import 'package:wisata_candi/screens/detail_screen.dart';
 
 class ItemCard extends StatelessWidget {
   // TODO: 1. Deklarasikan variabel yang dibutuhkan dan pasang pada konstruktor
   final Candi candi;
   const ItemCard({super.key, required this.candi});
+
   @override
   Widget build(BuildContext context) {
     // TODO: 6. Implementasi routing ke DetailScreen
-    return Card(
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DetailScreen(candi: candi),
+            ),
+          );
+      },
+    child: Card(
       // TODO: 2. Tetapkan parameter shape, margin, dan elevation dari Cari
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(15),
@@ -51,6 +62,7 @@ class ItemCard extends StatelessWidget {
           )
         ],
       ),
+     ),
     );
   }
 }
